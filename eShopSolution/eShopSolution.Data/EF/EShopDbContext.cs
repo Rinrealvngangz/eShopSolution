42be1aa7701b7ab9cb9836a7cc7ac945fce09a6f
Rinrealvngangz <tuannmctk42@gmail.com>
2021-01-02 23:25:32 +0700

    create EF

---
+++ eShopSolution/eShopSolution.Data/EF/EShopDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using eShopSolution.Data.Entities;
+namespace eShopSolution.Data.EF
+{
+    public class EShopDbContext : DbContext
+    {
+        public EShopDbContext() 
+        {
+
+        }
+
+        public EShopDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ eShopSolution/eShopSolution.Data/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using eShopSolution.Data.Enums;
+namespace eShopSolution.Data.Entities
+{
+    public class Category
+    {
+        
+        public int Id { get; set; }
+        public decimal SortOrder { get; set; }
+        public int IsShowOnHome { get; set; }
+        public int? ParentId { get; set; }
+        public Status Status  { get; set; }
+
+      
+   
+    }
+}
